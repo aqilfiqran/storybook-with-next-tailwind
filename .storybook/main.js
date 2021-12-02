6cfd84158378c30a04063e069c4a3bf9514fa7b0
aqilfiqran <aqilfiqran19@gmail.com>
2021-12-02 09:15:52 +0700

    feat(chore): integrate storybook and tailwind

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,10 +3,22 @@ module.exports = {
     "../stories/**/*.stories.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-controls",
+  ],
   framework: "@storybook/react",
   core: {
     builder: "webpack5",
   },
   typescript: { reactDocgen: false },
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+    });
+
+    return config;
+  },
 };
