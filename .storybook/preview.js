6d8786e035208cd0bb67574a2c39948f143399ae
aqilfiqran <aqilfiqran19@gmail.com>
2021-12-02 09:32:04 +0700

    fix(chore): Image component config in storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+import * as NextImage from "next/image";
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -8,3 +9,10 @@ export const parameters = {
     },
   },
 };
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
